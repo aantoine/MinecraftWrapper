a2948d6428242171f3d0233bb6d450308619fabd
aantoine <antoineagustin@gmail.com>
2015-09-21 22:44:46 -0300

    added javascript validation to the addJar form

--- servercraft/views/index/jar.php
+++ servercraft/views/index/jar.php
@@ -8,6 +8,10 @@
         var file = document.getElementById("uploadInput").value;
         var desc = document.getElementById("fileDescription").value;
 
+        if(!validateForm()){
+        	return;
+        }
+
         xhr.onreadystatechange = function() {
             if (xhr.readyState == 4 && xhr.status == 200) {
                 // Handle response.
@@ -26,12 +30,38 @@
 
 </script>
 
+<script type="text/javascript">
+function validateForm() {
+    var x = document.forms["form"]["myFiles"].value;
+    if (x == null || x == "") {
+        alert("Error: Jar Url must be filled out");
+        return false;
+    }
+    if(x.length>100){
+      alert("Error: Invalid Jar file Url");
+      return false;
+    }
+
+    var y = document.forms["form"]["fileDescription"].value;
+    if (y == null || y == "") {
+        alert("Error: Jar description must be filled out");
+        return false;
+    }
+    if(y.length>100){
+      alert("Error: Description is too long");
+      return false;
+    }
+    return true;
+
+}
+</script>
+
 
 
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
 
   <h1 class="page-header">Add new Jar</h1>
-  <form id="form" action="php/upload.php" method="POST">
+  <form id="form" action="#" method="POST">
 	  <table class="table table-striped">
 	  	<tbody>
 		    <tr>
